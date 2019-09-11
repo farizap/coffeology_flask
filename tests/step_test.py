c802a59ec8d41fd9550d03706d00b58ac480f9e3
adesupraptolaia <ades@alterra.id>
2019-09-11 20:10:53 +0700

    pycodestyle tests/step_test.py success

--- tests/step_test.py
+++ tests/step_test.py
@@ -1,5 +1,6 @@
 import json
-from . import app, client, cache, create_token_non_internal, create_token_internal, reset_database
+from . import app, client, cache, create_token_non_internal
+from . import create_token_internal, reset_database
 
 
 class TestStepCrud():
@@ -10,28 +11,30 @@ class TestStepCrud():
     def test_step_get_all_valid(self, client):
         # token = create_token_non_internal()
         data = {
-            'recipeID' : 1
+            'recipeID': 1
         }
-        res = client.get('/steps', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 200  
-    
+        res = client.get('/steps', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
+
     def test_step_get_all_invalid(self, client):
         # token = create_token_non_internal()
         data = {
-           'recipeID' : -1
+            'recipeID': -1
         }
-        res = client.get('/steps', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
+        res = client.get('/steps', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 404
 
 # step options
     def test_step_options_valid(self, client):
         # token = create_token_non_internal()
         res = client.options('/steps',
-                        content_type='application/json')
-        
-        res_json=json.loads(res.data)
+                             content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 200
