4ab24ebaae55505ece7d53318afddc720d0d576f
adesupraptolaia <ades@alterra.id>
2019-09-11 20:09:43 +0700

    pycodestyle tests/recipe_test.py success

--- tests/recipe_test.py
+++ tests/recipe_test.py
@@ -1,5 +1,6 @@
 import json
-from . import app, client, cache, create_token_non_internal, create_token_internal, reset_database
+from . import app, client, cache, create_token_non_internal
+from . import create_token_internal, reset_database
 
 
 class TestRecipeCrud():
@@ -10,100 +11,105 @@ class TestRecipeCrud():
     def test_recipe_get_by_id_valid(self, client):
         # token = create_token_non_internal()
         res = client.get('/recipes/1',
-                        content_type='application/json')
-        
-        res_json=json.loads(res.data)
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 200
 
     def test_recipe_get_by_id_invalid(self, client):
         # token = create_token_non_internal()
         res = client.get('/recipes/-1',
-                        content_type='application/json')
-        
-        res_json=json.loads(res.data)
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 404
 
 # recipe get all
     def test_recipe_get_all_valid_1(self, client):
         # token = create_token_non_internal()
         res = client.get('/recipes', content_type='application/json')
-        
-        res_json=json.loads(res.data)
+
+        res_json = json.loads(res.data)
         assert res.status_code == 200
 
     def test_recipe_get_all_valid_2(self, client):
         # token = create_token_non_internal()
         data = {
-            'userID' : 1,
-            'methodID' : 1,
-            'orderby' : 'favoriteCount',
-            'sort' : 'asc'
+            'userID': 1,
+            'methodID': 1,
+            'orderby': 'favoriteCount',
+            'sort': 'asc'
         }
-        res = client.get('/recipes', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 200  
-    
+        res = client.get('/recipes', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
+
     def test_recipe_get_all_valid_3(self, client):
         # token = create_token_non_internal()
         data = {
-            'userID' : 1,
-            'methodID' : 1,
-            'orderby' : 'favoriteCount',
-            'sort' : 'desc'
+            'userID': 1,
+            'methodID': 1,
+            'orderby': 'favoriteCount',
+            'sort': 'desc'
         }
-        res = client.get('/recipes', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 200  
+        res = client.get('/recipes', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
 
     def test_recipe_get_all_valid_4(self, client):
         # token = create_token_non_internal()
         data = {
-            'orderby' : 'difficulty',
-            'sort' : 'asc'
+            'orderby': 'difficulty',
+            'sort': 'asc'
         }
-        res = client.get('/recipes', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 200  
+        res = client.get('/recipes', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
 
     def test_recipe_get_all_valid_5(self, client):
         # token = create_token_non_internal()
         data = {
-            'orderby' : 'difficulty',
-            'sort' : 'desc'
+            'orderby': 'difficulty',
+            'sort': 'desc'
         }
-        res = client.get('/recipes', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 200  
+        res = client.get('/recipes', query_string=data,
+                         content_type='application/json')
+
+        res_json = json.loads(res.data)
+        assert res.status_code == 200
 
     def test_recipe_get_all_invalid(self, client):
         # token = create_token_non_internal()
         data = {
-            'userID' : -1,
-            'methodID' : -1
+            'userID': -1,
+            'methodID': -1
         }
-        res = client.get('/recipes', query_string=data, content_type='application/json')
-        
-        res_json=json.loads(res.data)
-        assert res.status_code == 404  
+        res = client.get('/recipes', query_string=data,
+                         content_type='application/json')
 
-    
+        res_json = json.loads(res.data)
+        assert res.status_code == 404
+
 # recipe options
+
     def test_recipe_options_by_id_valid(self, client):
         # token = create_token_non_internal()
         res = client.options('/recipes/1',
-                        content_type='application/json')
-        
-        res_json=json.loads(res.data)
+                             content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 200
 
     def test_recipe_options_valid(self, client):
         # token = create_token_non_internal()
         res = client.options('/recipes',
-                        content_type='application/json')
-        
-        res_json=json.loads(res.data)
+                             content_type='application/json')
+
+        res_json = json.loads(res.data)
         assert res.status_code == 200
