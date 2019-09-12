60967354f88a8fe6cac55346fdf1c7e515bd2451
farizap <fapriyanto@alterra.id>
2019-09-12 21:45:56 +0700

    edit login (add hash)

--- blueprints/auth/__init__.py
+++ blueprints/auth/__init__.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import jwt_required, get_jwt_claims
 from blueprints.user.model import Users
 from flask_cors import CORS
 import re
+import hashlib
 
 bp_auth = Blueprint('auth', __name__)
 api = Api(bp_auth)
@@ -30,47 +31,48 @@ def isValidPassword(password):
 
 
 class CreateTokenResources(Resource):
-
     def options(self):
         return {'code': 200, 'message': 'oke'}, 200
 
     def post(self):
+        '''User login'''
         parser = reqparse.RequestParser()
-        parser.add_argument('email',
-                            location='json', required=True)
-        parser.add_argument('password',
-                            location='json', required=True)
-        args = parser.parse_args()
+        parser.add_argument('email', location='json', required=True)
+        parser.add_argument('password', location='json', required=True)
+        body = parser.parse_args()
 
-        if isValidEmail(args['email']) is False:
+        if isValidEmail(body['email']) is False:
             return {'code': 400, 'message': 'Email is not valid'}, 400
 
-        if isValidPassword(args['password']) is False:
+        if isValidPassword(body['password']) is False:
             return {'code': 400, 'message': 'Password is not valid'}, 400
 
-        userQry = Users.query
+        # Hash inputtted password
+        passwordHashed = hashlib.md5(body['password'].encode()).hexdigest()
 
-        userQry = userQry.filter_by(email=args['email'])
-        userQry = userQry.filter_by(password=args['password']).first()
+        userQry = Users.query
+        userQry = userQry.filter_by(email=body['email'])
+        userQry = userQry.filter_by(password=passwordHashed).first()
 
         if userQry is not None:
             user_data = marshal(userQry, Users.responseFieldsJwt)
-            token = create_access_token(
-                identity=args['email'], user_claims=user_data)
+            token = create_access_token(identity=body['email'],
+                                        user_claims=user_data)
         else:
-            return {'code': 401,
-                    'message': 'UNATHORIZED invalid email or password'}, 401
+            return {
+                'code': 401,
+                'message': 'UNATHORIZED invalid email or password'
+            }, 401
         return {'code': 200, 'message': 'oke', 'data': token}, 200
 
 
 class RefreshTokenResources(Resource):
-
     @jwt_required
     def post(self):
         current_user = get_jwt_identity()
         user_data = get_jwt_claims()
-        token = create_access_token(
-            identity=current_user, user_claims=user_data)
+        token = create_access_token(identity=current_user,
+                                    user_claims=user_data)
         return {'code': 200, 'message': 'oke', 'data': token}, 200
 
 
