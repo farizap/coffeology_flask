77ac1ffb0ad1c0d23038095ac37bf21494ff27c8
nadayasinta <nada@alterra.id>
2019-09-21 15:49:42 +0700

    bean get edit

--- blueprints/bean/resources.py
+++ blueprints/bean/resources.py
@@ -19,12 +19,41 @@ class BeanResource(Resource):
         return {'code': 200, 'message': 'oke'}, 200
 
     def get(self, id):
-        bean = Beans.query.get(id)
+        # bean = Beans.query.get(id)
+        bean="as"
         if bean is not None:
+            # beanData = marshal(bean, Beans.responseFields)
+            beanData = {
+                'id': 1,
+                'originID': 1,
+                'name': "bean1",
+                'photo':
+                    "http://3.bp.blogspot.com/-NlbLDQ72yfg/VgLQFnMkCSI/AAAAAAAADaY/eiX1XdNv0uI/s1600/kopiaceh.jpg",
+                'fragrance': 0.4,
+                'flavor': 0.4,
+                'aftertaste': 0.5,
+                'acidity': 0.5,
+                'body': 0.5,
+                'balance': 0.5,
+                'uniformity': 0.3,
+                'cleanCups': 0.5,
+                'sweetness': 0.3,
+                'overall': 0.4,
+                'description':
+                    "Coffee trees are planted in Jernih Jaya Village locate Arabica Simalungun Location: North Sumatera cupping: September 2017 by Gayo Cuppers Team in Gunung Tujuh, Kerinci District, Jambi Province. Coffee plantation are grown in the altitute od 1,200 - 1,400 meter above sea level in the Mount Kerinci areas. Beside of coffee, the location is well known for agro tourism.",
+                'cupping': "cupping",
+                'advantage': "advantage1,gggadvantage1",
+                'disadvantage': "disadvantage1,disadvantage2,disadvantage3",
+                'location': "location"
+            },
+            beanAdvantages = beanData['advantage']
+            beanData['advantage'] = beanAdvantages.split(',')
+            beanDisadvantages = beanData['disadvantage']
+            beanData['advantdisadvantageage'] = beanDisadvantages.split(',')
             return {
                 'code': 200,
                 'message': 'oke',
-                'data': marshal(bean, Beans.responseFields)
+                'data': beanData
             }, 200
         return {'code': 404, 'message': 'Bean Not Found'}, 404
 
